af3b1bb868c4bbf09f143e404248db4a65473b88
Arnold De Guzman <deguzman.arnold1005@gmail.com>
2019-03-07 07:28:33 -0800

    ewan na

--- www/js/process.js
+++ www/js/process.js
@@ -1,16 +1,9 @@
 let process = {
   menu:function(){
     let content = <div className="page">
-      <iframe src="http://192.168.0.102" width="100%" height="100%" onerror={process.error()}></iframe>
+      <iframe src="http://192.168.0.102" width="100%" height="100%"></iframe>
     </div>;
     ReactDOM.render(content,document.getElementById('root'));
-  
-  },
-  error:function(){
-    let content = <div className="page">
-    <h3>The server is unavailable. Check if the device is connected to the Router.</h3>
-  </div>;
-  ReactDOM.render(content,document.getElementById('root'));
   },
   loading:function(){
     let content = <div>
@@ -23,4 +16,4 @@ let process = {
 process.loading();
 setTimeout(function(){
   process.menu();
-},2000);
+},7000);
